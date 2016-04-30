17b9d6db88c534cf5b35652595ac3624eb608722
demodawid <demodawid@gmail.com>
2016-04-30 14:26:42 -0300

    Tabla dias y comento los insert

--- 20161Q_GR04/SCRIPT_CREACION_TABLAS.sql
+++ 20161Q_GR04/SCRIPT_CREACION_TABLAS.sql
@@ -1,6 +1,9 @@
 USE [MIS_GRUPO_04]
 GO
 
+IF OBJECT_ID('dbo.LK_Dia', 'U') IS NOT NULL 
+  DROP TABLE dbo.LK_Dia; 
+
 IF OBJECT_ID('dbo.LK_Causa', 'U') IS NOT NULL 
   DROP TABLE dbo.LK_Causa; 
   
@@ -63,7 +66,7 @@ CREATE TABLE [dbo].[LK_Anio](
 
 
 --Inserto los anios
-INSERT INTO dbo.LK_Anio (Anio_numero) VALUES(2013),(2014),(2015),(2016),(2017)
+--INSERT INTO dbo.LK_Anio (Anio_numero) VALUES(2013),(2014),(2015),(2016),(2017)
 
 CREATE TABLE [dbo].[LK_Mes](
 	[Mes_SK] [int] IDENTITY(1,1) NOT NULL,
@@ -85,30 +88,47 @@ ALTER TABLE [dbo].[LK_Mes] CHECK CONSTRAINT [FK_LK_Mes_LK_Anio]
 GO
 
 --Inserto los meses de cada anio
-INSERT INTO dbo.LK_Mes (Mes_Numero, Anio_SK)
-	SELECT 1, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 2, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 3, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 4, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 5, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 6, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 7, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 8, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 9, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 10, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 11, Anio_SK FROM dbo.LK_Anio
-	UNION ALL
-	SELECT 12, Anio_SK FROM dbo.LK_Anio
+--INSERT INTO dbo.LK_Mes (Mes_Numero, Anio_SK)
+--	SELECT 1, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 2, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 3, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 4, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 5, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 6, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 7, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 8, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 9, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 10, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 11, Anio_SK FROM dbo.LK_Anio
+--	UNION ALL
+--	SELECT 12, Anio_SK FROM dbo.LK_Anio
+
+
+CREATE TABLE [dbo].[LK_Dia](
+	[Dia_SK] [int] IDENTITY(1,1) NOT NULL,
+	[Dia_Numero] [int] NOT NULL,
+	[Dia_DiaSemana] [int] NOT NULL,
+	[Mes_Sk] [int] NOT NULL,
+ CONSTRAINT [PK_LK_Dia] PRIMARY KEY CLUSTERED 
+(
+	[Dia_SK] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
+
 
 -------------------------------------------------------------
 ------ INSERTAR ACA EL RESTO DE LA DIM TIEMPO ---------------
